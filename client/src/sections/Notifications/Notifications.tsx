30a0e77f10c70dc1c0796834f9cac8255f608242
hironico <hironico.pub@outlook.com>
2024-12-02 21:25:26 +0100

    titre de la blague en francais

--- client/src/sections/Notifications/Notifications.tsx
+++ client/src/sections/Notifications/Notifications.tsx
@@ -15,7 +15,7 @@ const CustomNotification = forwardRef(function CustomNotification(
 ) {
   return (
     <Alert ref={ref} severity="info">
-      <AlertTitle>Notification demo (random IT jokes :))</AlertTitle>
+      <AlertTitle>Une blague au hasard :)</AlertTitle>
       {message}
     </Alert>
   );
