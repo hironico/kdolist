c2d8683bf6f2add668f7b20c61e029adb21dcad1
hironico <hironico.pub@outlook.com>
2024-12-08 09:32:49 +0100

    Meilleure page d'accueil

--- client/src/pages/Welcome/Welcome.tsx
+++ client/src/pages/Welcome/Welcome.tsx
@@ -1,67 +1,193 @@
+import React, { ReactElement } from 'react';
+import { Box, Button, Container, Typography, Paper } from '@mui/material';
+import Carousel from 'react-material-ui-carousel';
+import { styled } from '@mui/material/styles';
+import AutoFixHighRoundedIcon from '@mui/icons-material/AutoFixHighRounded';
+import ThumbUpAltRoundedIcon from '@mui/icons-material/ThumbUpAltRounded';
+import QuestionAnswer from '@mui/icons-material/QuestionAnswer';
+import PsychologyAltIcon from '@mui/icons-material/PsychologyAlt';
 import Meta from '@/components/Meta';
-import Content from './Content';
-import Stack from '@mui/material/Stack';
-import Box from '@mui/material/Box';
-import { useContext } from 'react';
-import { LoginContext } from '@/LoginContext';
-import { Navigate, useNavigate } from 'react-router-dom';
-import { Button, Typography } from '@mui/material';
-import { CenteredFlexBox } from '@/components/styled';
+import { useNavigate } from 'react-router-dom';
 
-function Welcome() {
-  // const isPortrait = useOrientation();
-  const loginContext = useContext(LoginContext);
-
-  const navigate = useNavigate();
-  const onLetsGo = () => {
-    navigate('/login', { replace: true });
-  }
-
-  /*
-  const width = isPortrait ? '40%' : '30%';
-  const height = isPortrait ? '30%' : '40%';
-  */
-
-  const welcomePage = (
-    <>
-      <Meta title="Welcome" />
-      <Box sx={{
-        backgroundColor: 'transparent', // Set the background color to transparent
-        backgroundImage: 'url(./background/kdolist-1.jpeg)', // Set the background image
-        backgroundSize: 'cover', // Set the background image size to cover the entire component
-        backgroundPosition: 'center',
-        height: { xs: '100%', md: '100%'},
-        overflowY: 'scroll'
-      }}>
-        <Stack
-          direction="column"
-          component="main"
-          gap={2}
-          sx={[
-            {
-              justifyContent: 'flex-start',
-              height: { xs: 'auto', md: '100%' },
-              marginLeft: {xs: '0px', md: '10px'},
-              marginRight: {xs: '0px', md: '10px'}
-            },
-          ]}
-        >
-          <CenteredFlexBox>
-            <Typography variant='h2' color="lightgray">K.DO-List</Typography>
-          </CenteredFlexBox>
-          <Content />
-          <CenteredFlexBox sx={{marginTop: '15px'}} alignItems={"center"}>
-            <Button variant='contained' onClick={(_evt) => onLetsGo()} sx={{padding: '10px', margin: '5px', width: '100%', maxWidth: '850px'}}>C&apos;est parti !</Button>
-          </CenteredFlexBox>
-          <CenteredFlexBox alignItems={"center"} sx={{marginBottom: '30px', marginTop: '10px'}}>
-          <Button variant='contained' color="secondary" href="https://kdolist.hironico.net/legal/privacy" sx={{margin: '5px', padding: '10px', width: '100%', maxWidth: '850px'}}>Politique de confidentialité</Button>
-          </CenteredFlexBox>
-        </Stack>
-      </Box>
-    </>
-  );
-
-  return loginContext.loginInfo.jwt != '' ? <Navigate to="/mylists" replace /> : welcomePage;
+interface CarouselItem {
+    image: string;
+    title: string;
+    description: string;
+    buttonText: string;
 }
+
+interface FeatureCard {
+    icon: ReactElement;
+    title: string;
+    description: string;
+}
+
+const CarouselContainer = styled(Box)(({ theme }) => ({
+    position: 'relative',
+    backgroundColor: 'transparent',
+    minHeight: '55vh',
+}));
+
+const CarouselSlide = styled(Paper)(({ theme }) => ({
+    height: '50vh',
+    position: 'relative',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: theme.spacing(2),
+    color: '#fff',
+    '&::before': {
+        content: '""',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        //backgroundColor: 'rgba(0,0,0,0.5)',
+        zIndex: 1,
+    },
+}));
+
+const SlideContent = styled(Box)(({ theme }) => ({
+    position: 'relative',
+    zIndex: 2,
+    textAlign: 'center',
+    backgroundColor: 'transparent',
+    borderColor: 'transparent'
+}));
+
+const FeatureGrid = styled(Box)(({ theme }) => ({
+    display: 'grid',
+    gridTemplateColumns: 'repeat(1, 1fr)',
+    gap: theme.spacing(2),
+    padding: theme.spacing(1),
+    [theme.breakpoints.up('md')]: {
+        gridTemplateColumns: 'repeat(2, 1fr)',
+    },
+}));
+
+const FeatureBox = styled(Paper)(({ theme }) => ({
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    height: '100%',
+}));
+
+const carouselItems: CarouselItem[] = [
+    {
+        image: './background/kdolist-1.jpeg',
+        title: 'K.DO-List',
+        description: 'Notre nouvelle façon de partager nos listes de cadeaux',
+        buttonText: 'Commencer',
+    },
+    // Ajoutez d'autres éléments du carousel ici
+];
+
+const features: FeatureCard[] = [
+    {
+        icon: <QuestionAnswer fontSize="small" sx={{ color: 'text.secondary' }} />,
+        title: 'C\'est quoi?',
+        description:
+            'K.DO-LIST gère tes listes de cadeaux. Partages les avec ta famille ou tes amis.',
+    },
+    {
+        icon: <PsychologyAltIcon sx={{ color: 'text.secondary' }} />,
+        title: 'Comment ça marche?',
+        description:
+            'Utilises un réseau social pour te faire connaitre, puis crées ta premiere liste. Tu n\'es pas sur les réseaux? \
+        Fais-le nous savoir et on s\'occuppe de tout.'
+    },
+    {
+        icon: <ThumbUpAltRoundedIcon sx={{ color: 'text.secondary' }} />,
+        title: 'Enfin prêt pour le mobile',
+        description:
+            'Ranges ton téléscope, K.DO-LIST est adapté pour l\'affichage sur mobile.',
+    },
+    {
+        icon: <AutoFixHighRoundedIcon sx={{ color: 'text.secondary' }} />,
+        title: 'Une fonctionnalité manque?',
+        description:
+            'On a déjà plein d\'idées et si peu de temps pour les mettre en place, mais parles-en dans le chat et on va voir ce qu\'on peut faire, rien que pour toi.',
+    },
+];
+
+const Welcome: React.FC = () => {
+
+    const navigate = useNavigate();
+    const onLetsGo = () => {
+        navigate('/login', { replace: true });
+    }
+
+    return (
+        <>
+            <Meta title="Welcome" />
+            <Container maxWidth="lg" sx={{ pb: 2, overflowY: 'scroll', height: '100%' }}>
+                <CarouselContainer>
+                    <Carousel
+                        animation="slide"
+                        indicators={true}
+                        navButtonsAlwaysVisible={true}
+                        cycleNavigation={true}
+                        fullHeightHover={true}
+                        sx={{ height: '55vh' }}
+                    >
+                        {carouselItems.map((item, index) => (
+                            <CarouselSlide
+                                key={index}
+                                sx={{
+                                    backgroundImage: `url(${item.image})`,
+                                }}
+                            >
+                                <SlideContent>
+                                    <Typography variant="h4" component="h1" gutterBottom>
+                                        {item.title}
+                                    </Typography>
+                                    <Typography variant="subtitle1" gutterBottom>
+                                        {item.description}
+                                    </Typography>
+                                    <Button
+                                        variant="contained"
+                                        color="primary"
+                                        size="medium"
+                                        sx={{ mt: 4, width: '100%' }}
+                                        onClick={(_e) => onLetsGo()}
+                                    >
+                                        {item.buttonText}
+                                    </Button>
+                                </SlideContent>
+                            </CarouselSlide>
+                        ))}
+                    </Carousel>
+                </CarouselContainer>
+
+                <FeatureGrid>
+                    {features.map((feature, index) => (
+                        <FeatureBox key={index} elevation={2}>
+                            <Typography variant="h1" component="div" sx={{ fontSize: '2rem', mb: 1 }}>
+                                {feature.icon}
+                            </Typography>
+                            <Typography variant="h6" component="h2" gutterBottom>
+                                {feature.title}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                {feature.description}
+                            </Typography>
+                        </FeatureBox>
+                    ))}
+                </FeatureGrid>
+                <Button variant="contained"
+                    color="primary"
+                    size="medium"
+                    onClick={(_e) => onLetsGo()}
+                    sx={{ mt: 4, width: '100%' }}                    
+                >
+                    C'est parti !
+                </Button>
+            </Container>
+        </>
+    );
+};
 
 export default Welcome;
