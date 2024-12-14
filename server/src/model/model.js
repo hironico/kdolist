854a926665ecaed9ee04ef9fa6de36ec660bc893
hironico <hironico.pub@outlook.com>
2024-12-14 21:02:21 +0100

    added log message to check the connectivity

--- server/src/model/model.js
+++ server/src/model/model.js
@@ -2,6 +2,8 @@ const { Sequelize, DataTypes, Model } = require('sequelize');
 const dotenv = require('dotenv').config();
 const logger = require('../logger');
 
+logger.info(`Connecting to database : ${process.env.DB_NAME} on ${process.env.DB_HOSTNAME}:${process.env.DB_PORT} with user ${process.env.DB_USER}`);
+
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOSTNAME,
   port:process.env.DB_PORT,
