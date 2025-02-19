0a21c2d6cf91183d16c1005d314cb1dcae6ebff5
hironico <hironico.pub@outlook.com>
2025-02-19 18:05:51 +0100

    fix maximum size of json payload to 5 mb

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const https = require('https');
 const http = require('http');
 const fs = require('fs');
+const bodyParser = require('body-parser');
 
 // configure application with process env variables from .env file
 require('dotenv').config();
@@ -17,7 +18,7 @@ var corsOptions = {
 
 // Middlewares
 app.use(cors(corsOptions));
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(process.env.WEBUI_HOME_DIR)); // Serve static files from the 'public' directory see .env file
 
