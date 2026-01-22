0f0ebaa11a31366b0fc77c27b86644b2ea2acccb
hironico <hironico.pub@outlook.com>
2026-01-22 08:46:40 +0100

    New log configuration to avoir colored message in files resulting in non pritable characters

--- server/src/logger.js
+++ server/src/logger.js
@@ -5,9 +5,8 @@ const dotenv = require('dotenv');
 // make sure the config is loaded
 dotenv.config();
 
-const logger = winston.createLogger({
-  level: process.env.LOG_LEVEL,
-  format: winston.format.combine(
+// formats used to print log messages
+const colorFormat = winston.format.combine(
     winston.format.colorize(), 
     winston.format.timestamp({
       format: 'YYYY-MM-DD HH:mm:ss',
@@ -15,27 +14,57 @@ const logger = winston.createLogger({
     winston.format.printf((info) => {
       return `${info.timestamp} - ${info.level}: ${info.message}`;
     }),
-  ),
-  transports: [
-    new winston.transports.Console({
-      handleExceptions: true,
-      level: process.env.LOG_LEVEL,
+  );
+
+const fileFormat = winston.format.combine(
+    winston.format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss',
     }),
-    new DailyRotateFile({
-      filename: './logs/combined-%DATE%.log',
+    winston.format.printf((info) => {
+      return `${info.timestamp} - ${info.level}: ${info.message}`;
+    })
+  );
+
+// transports where to ouput log messages into files and/or console
+const transports = [];
+
+const logDir = process.env.LOG_DIR;
+
+const combinedTransort = new DailyRotateFile({
+      filename: `${logDir}/kdolist-server-combined-%DATE%.log`,
       datePattern: 'YYYY-MM-DD',
       zippedArchive: true,
       handleExceptions: true,
       level: process.env.LOG_LEVEL,
-    }),
-    new DailyRotateFile({
-      filename: './logs/exceptions-%DATE%.log',
+      format: fileFormat,
+    });
+
+const exceptionTransport = new DailyRotateFile({
+      filename: `${logDir}/kdolist-server-exceptions-%DATE%.log`,
       datePattern: 'YYYY-MM-DD',
       zippedArchive: true,
       handleExceptions: true,
       level: 'error',
-    }),
-  ],
+      format: fileFormat,
+    });
+
+transports.push(combinedTransort);
+transports.push(exceptionTransport);
+
+// there is an option to disable log in the console
+if (process.env.LOG_CONSOLE_DISABLED !== 'true') {
+  const consoleTransport = new winston.transports.Console({      
+      handleExceptions: true,
+      level: process.env.LOG_LEVEL,
+      format: colorFormat,
+    });
+
+    transports.push(consoleTransport);
+}
+
+const logger = winston.createLogger({
+  level: process.env.LOG_LEVEL,
+  transports: transports,
 });
 
 module.exports = logger;
